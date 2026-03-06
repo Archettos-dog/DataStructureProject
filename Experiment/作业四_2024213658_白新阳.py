6187a3a13a9b04e9fc9d8d4e7447d9082a71af67
Bai Xinyang <phoenixblaze31@gmail.com>
2026-03-06 19:30:07 +0800

    工具函数

--- Experiment/作业四_2024213658_白新阳.py
+++ Experiment/作业四_2024213658_白新阳.py
@@ -19,3 +19,31 @@ X_val   = scaler.transform(X_val)
 N_train, D = X_train.shape   # D = 64
 H = 128                       # 隐藏层维度
 C = 10                        # 类别数
+
+#2.工具函数
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_grad(x):
+    return (x > 0).astype(float)
+
+def softmax(x):
+    x = x - x.max(axis=1, keepdims=True)   # 数值稳定
+    e = np.exp(x)
+    return e / e.sum(axis=1, keepdims=True)
+
+def cross_entropy_loss(probs, y):
+    """平均交叉熵损失"""
+    N = len(y)
+    log_probs = -np.log(probs[np.arange(N), y] + 1e-12)
+    return log_probs.mean()
+
+def accuracy(probs, y):
+    return (probs.argmax(axis=1) == y).mean()
+
+def one_hot(y, C):
+    N = len(y)
+    oh = np.zeros((N, C))
+    oh[np.arange(N), y] = 1.0
+    return oh
+
